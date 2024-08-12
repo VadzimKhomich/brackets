2737f94463b8b216c7826f730fd66dc6fb7aaf14
vadzim khomich <vadzim.khomich@activeplatform.com>
2024-08-12 15:25:46 +0300

    done brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+      let objBracketsConfig = Object.fromEntries(bracketsConfig)
+    let objKeys = Object.keys(objBracketsConfig)
+    let objValues = Object.values(objBracketsConfig)
+
+    let result = []
+
+    for (let i = 0; i < str.length; i++) {
+        let currentBracket = str[i]
+        let lastElement = result[result.length - 1]
+        if (objKeys.includes(currentBracket)) {
+            if ((lastElement === currentBracket) && (objBracketsConfig[currentBracket] === currentBracket)) {
+                result.pop()
+            } else {
+                result.push(currentBracket)
+            }
+        } else if (objValues.includes(currentBracket)) {
+            if(objBracketsConfig[lastElement] === currentBracket) {
+                result.pop()
+            } else {
+                return false
+            }
+        }
+    }
+
+    return (result.length === 0)
 }
